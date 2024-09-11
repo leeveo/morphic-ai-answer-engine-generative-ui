61b1e74ba0ac102e5f90e08a494be9d5cdb9f288
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-11 14:02:27 +0200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -6,18 +6,18 @@ import HistoryContainer from './history-container'
 
 export const Header: React.FC = async () => {
   return (
-    <header className="fixed w-full p-1 md:p-2 flex flex-col justify-center items-center z-10 backdrop-blur md:backdrop-blur-none bg-background/80 md:bg-transparent">
-      <div className="flex flex-col items-center">
+    <header className="fixed w-full p-1 md:p-2 flex justify-between items-center z-10 backdrop-blur md:backdrop-blur-none bg-background/80 md:bg-transparent">
+      <div>
         <a href="/">
           <IconLogo className={cn('w-5 h-5')} />
           <span className="sr-only">Anne Kerdy</span>
         </a>
-        {/* Phrase d'introduction centrée */}
-        <p className="text-lg font-semibold mt-2 text-black text-center">
-          Bienvenue sur mon site ! Découvrez mes projets et mon parcours.
+        {/* Phrase d'introduction */}
+        <p className="text-lg font-semibold mt-2">
+        Anne Kerdi répond à toutes vos questions sur </br> la Bretagne
         </p>
       </div>
-      <div className="flex gap-0.5 mt-4">
+      <div className="flex gap-0.5">
         <ModeToggle />
         <HistoryContainer location="header" />
       </div>

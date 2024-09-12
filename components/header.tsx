75ac74cbb70a7ebbfe57be44cb5383f929f158b1
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-12 14:07:59 +0200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -8,7 +8,7 @@ export const Header: React.FC = async () => {
   return (
     <header
       className="fixed w-full p-1 md:p-2 flex justify-between items-center z-10 backdrop-blur md:backdrop-blur-none bg-background/80 md:bg-transparent"
-      style={{ backgroundImage: 'url(/images/high-angle-view-of-the-beach-of-saint-malo-2023-11-27-04-51-01-utc.jpg)', backgroundSize: 'cover', backgroundPosition: 'center', height: '300px' }}
+      style={{ backgroundImage: 'url(/images/high-angle-view-of-the-beach-of-saint-malo-2023-11-27-04-51-01-utc.jpg)', backgroundSize: 'cover', backgroundPosition: 'center', height: '50px' }}
     >
       <div className="flex items-center">
         <a href="/">

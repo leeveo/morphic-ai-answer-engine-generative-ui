1a763b6a5c1818ac2b2e0894a7ad54fd606fc611
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-12 12:39:22 +0200

    ajout header

--- components/header.tsx
+++ components/header.tsx
@@ -7,11 +7,12 @@ import HistoryContainer from './history-container'
 export const Header: React.FC = async () => {
   return (
     <header className="fixed w-full p-1 md:p-2 flex justify-between items-center z-10 backdrop-blur md:backdrop-blur-none bg-background/80 md:bg-transparent">
-      <div>
+      <div className="flex items-center">
         <a href="/">
           <IconLogo className={cn('w-5 h-5')} />
           <span className="sr-only">Anne Kerdy</span>
         </a>
+        <p className="ml-4 text-sm font-semibold">Bienvenue en Bretagne!</p>
       </div>
      
       <div className="flex gap-0.5">

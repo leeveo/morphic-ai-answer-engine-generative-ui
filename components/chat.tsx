70fb695b48f122e34f881de0dc0befda9832f8ff
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-12 13:45:50 +0200

    Update chat.tsx

--- components/chat.tsx
+++ components/chat.tsx
@@ -21,11 +21,10 @@ export function Chat({ id, query }: ChatProps) {
     }
   }, [id, path, messages, query])
 
-    return (
-    <div className="px-8 sm:px-12 pt-12 md:pt-14 pb-14 md:pb-24 max-w-3xl mx-auto flex flex-col space-y-3 md:space-y-4" style={{ marginTop: '300px' }}>
+  return (
+    <div className="px-8 sm:px-12 pt-12 md:pt-14 pb-14 md:pb-24 max-w-3xl mx-auto flex flex-col space-y-3 md:space-y-4">
       <ChatMessages messages={messages} />
       <ChatPanel messages={messages} query={query} />
     </div>
-  )
   )
 }

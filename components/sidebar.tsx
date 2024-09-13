3522d4e05c63352eb5b72b0054cb74590df9d010
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-13 18:35:12 +0200

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,11 +2,11 @@ import HistoryContainer from './history-container'
 
 export async function Sidebar() {
   return (
-    <div className="h-screen p-2 fixed top-0 left-0 flex-col justify-center pb-24 hidden sm:flex" style={{ width: '450px', marginTop: '80px' }}>
+    <div className="h-screen p-2 fixed top-0 left-0 flex-col justify-center pb-24 hidden sm:flex" style={{ width: '450px', marginTop: '150px' }}>
       <div className="flex flex-col items-center space-y-4">
-        <img src="https://www.leeveo.tv/wp-content/uploads/2023/03/animation-montgolfiere-realite-virtuelle-1024x768.jpg" alt="Anne 1" className="w-450 h-270 object-cover" />
-        <img src="https://leeveo.tv/wp-content/uploads/2023/03/smulateur_vr.jpg" alt="Anne 2" className="w-450 h-270 object-cover" />
-        <img src="https://www.leeveo.tv/wp-content/uploads/2023/03/simulateur-rollercoaster-1024x768.jpg" alt="Anne 3" className="w-450 h-270 object-cover" />
+        <img src="https://www.leeveo.tv/wp-content/uploads/2024/09/image001.jpg" alt="Anne 1" className="w-450 h-270 object-cover" />
+        <img src="https://www.leeveo.tv/wp-content/uploads/2024/09/image002.jpg" alt="Anne 2" className="w-450 h-270 object-cover" />
+        <img src="https://www.leeveo.tv/wp-content/uploads/2024/09/image003.jpg" alt="Anne 3" className="w-450 h-270 object-cover" />
       </div>
       <HistoryContainer location="sidebar" />
     </div>

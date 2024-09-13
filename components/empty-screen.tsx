d6facc5d3e1128fc4cc183f62c4bbfb5ba6e0a7b
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-13 11:14:27 +0200

    Update empty-screen.tsx

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -1,6 +1,3 @@
-import { Button } from '@/components/ui/button'
-import { ArrowRight } from 'lucide-react'
-
 const exampleMessages = [
   {
     heading: 'Quelle est ta plage bretonne préférée et pourquoi ?',
@@ -32,8 +29,9 @@ export function EmptyScreen({
       <div className="bg-background p-2">
         <div className="mt-4 flex flex-col items-start space-y-2 mb-4">
           <img src="/images/anne001.png" alt="Placeholder" className="w-64 h-64 object-cover rounded-full mx-auto" />
-                    <p className="text-sm font-semibold italic text-center">Je m'appelle Anne Kerdi. Je suis une passionnée de la Bretagne et je partage mes découvertes sur la vie locale, la culture, le patrimoine et la gastronomie de cette magnifique région.</p>
-                    {exampleMessages.map((message, index) => (
+          <p className="text-sm font-semibold italic text-center">Je m&apos;appelle Anne Kerdi. Je suis une passionnée de la Bretagne et je partage mes découvertes sur la vie locale, la culture, le patrimoine et la gastronomie de cette magnifique région.</p>
+          <p className="text-base font-semibold">Voici quelques questions pour démarrer :</p>
+          {exampleMessages.map((message, index) => (
             <Button
               key={index}
               variant="link"

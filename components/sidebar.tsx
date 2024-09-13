f1371c373985654d21b8ef486e8392750a441e45
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-13 18:30:36 +0200

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,9 +4,9 @@ export async function Sidebar() {
   return (
     <div className="h-screen p-2 fixed top-0 left-0 flex-col justify-center pb-24 hidden sm:flex" style={{ width: '450px', marginTop: '80px' }}>
       <div className="flex flex-col items-center space-y-4">
-        <img src="/images/envcartpub001.jpg" alt="Anne 1" className="w-450 h-270 object-cover" />
-        <img src="/images/envcartpub001.jpg" alt="Anne 2" className="w-450 h-270 object-cover" />
-        <img src="/images/envcartpub001.jpg" alt="Anne 3" className="w-450 h-270 object-cover" />
+        <img src="https://www.leeveo.tv/wp-content/uploads/2023/03/animation-montgolfiere-realite-virtuelle-1024x768.jpg" alt="Anne 1" className="w-450 h-270 object-cover" />
+        <img src="https://leeveo.tv/wp-content/uploads/2023/03/smulateur_vr.jpg" alt="Anne 2" className="w-450 h-270 object-cover" />
+        <img src="https://www.leeveo.tv/wp-content/uploads/2023/03/simulateur-rollercoaster-1024x768.jpg" alt="Anne 3" className="w-450 h-270 object-cover" />
       </div>
       <HistoryContainer location="sidebar" />
     </div>

aa782c9d72be72754a8ad965e0c55e814b9b5669
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-13 11:45:07 +0200

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,7 +2,7 @@ import HistoryContainer from './history-container'
 
 export async function Sidebar() {
   return (
-    <div className="h-screen p-2 fixed top-0 left-0 flex-col justify-center pb-24 hidden sm:flex" style={{ width: '250px' }}>
+    <div className="h-screen p-2 fixed top-0 left-0 flex-col justify-center pb-24 hidden sm:flex" style={{ width: '350px' }}>
       <div className="flex flex-col items-center space-y-4">
         <img src="/images/envcartpub001.jpg" alt="Anne 1" className="w-48 h-48 object-cover" />
         <img src="/images/envcartpub001.jpg" alt="Anne 2" className="w-48 h-48 object-cover" />

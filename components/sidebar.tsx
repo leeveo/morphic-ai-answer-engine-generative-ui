09c8813ceb4fd8a2b63eda1e523caabff2a23dd3
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-14 11:25:24 +0200

    hauteur

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,7 +3,7 @@ import HistoryContainer from './history-container';
 export async function Sidebar() {
   return (
     <div className="h-screen p-2 fixed top-0 left-0 flex flex-col justify-center pb-24 hidden sm:flex sidebar-container w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4" 
-         style={{ marginTop: '100px' }}>
+         style={{ marginTop: '360px' }}>
       <div className="flex flex-col items-center space-y-4 w-full">
         {/* Image 1 */}
         <img 

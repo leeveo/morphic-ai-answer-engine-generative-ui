0157647cc6482b8a91a8ac2618d3e5e1244d9ec1
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-14 10:24:51 +0200

    revert

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,31 +1,14 @@
-"use client";
+import HistoryContainer from './history-container'
 
-import React, { useEffect, useState } from 'react';
-import HistoryContainer from './history-container';
-
-export function Sidebar() {
-  const [images, setImages] = useState<string[]>([]);
-
-  useEffect(() => {
-    fetch('/random-images')
-      .then(response => response.json())
-      .then(data => {
-        console.log('Images fetched:', data); // Log the fetched images
-        setImages(data);
-      })
-      .catch(error => {
-        console.error('Error fetching images:', error);
-      });
-  }, []);
-
+export async function Sidebar() {
   return (
     <div className="h-screen p-2 fixed top-0 left-0 flex-col justify-center pb-24 hidden sm:flex" style={{ width: '450px', marginTop: '150px' }}>
       <div className="flex flex-col items-center space-y-4">
-        {images.map((image, index) => (
-          <img key={index} src={`/images/${image}`} alt={`Random ${index + 1}`} className="w-450 h-270 object-cover" />
-        ))}
+        <img src="https://www.leeveo.tv/wp-content/uploads/2024/09/image001.jpg" alt="Anne 1" className="w-450 h-270 object-cover" />
+        <img src="https://www.leeveo.tv/wp-content/uploads/2024/09/image002.jpg" alt="Anne 2" className="w-450 h-270 object-cover" />
+        <img src="https://www.leeveo.tv/wp-content/uploads/2024/09/image003.jpg" alt="Anne 3" className="w-450 h-270 object-cover" />
       </div>
       <HistoryContainer location="sidebar" />
     </div>
-  );
+  )
 }

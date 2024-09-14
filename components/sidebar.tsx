593a39354511b48b943ae9eb99f94fa6edd1c6b5
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-14 10:31:01 +0200

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,11 +2,29 @@ import HistoryContainer from './history-container'
 
 export async function Sidebar() {
   return (
-    <div className="h-screen p-2 fixed top-0 left-0 flex-col justify-center pb-24 hidden sm:flex" style={{ width: '450px', marginTop: '150px' }}>
+    <div className="h-screen p-2 fixed top-0 left-0 flex-col justify-center pb-24 hidden sm:flex" style={{ width: '450px', marginTop: '100px' }}>
       <div className="flex flex-col items-center space-y-4">
-        <img src="https://www.leeveo.tv/wp-content/uploads/2024/09/image001.jpg" alt="Anne 1" className="w-450 h-270 object-cover" />
-        <img src="https://www.leeveo.tv/wp-content/uploads/2024/09/image002.jpg" alt="Anne 2" className="w-450 h-270 object-cover" />
-        <img src="https://www.leeveo.tv/wp-content/uploads/2024/09/image003.jpg" alt="Anne 3" className="w-450 h-270 object-cover" />
+        {/* Image 1 */}
+        <img 
+          src="https://www.leeveo.tv/wp-content/uploads/2024/09/image001.jpg" 
+          alt="Anne 1" 
+          className="w-full h-auto max-w-full object-cover"
+          style={{ maxHeight: '300px' }}  // Limite la hauteur si nécessaire
+        />
+        {/* Image 2 */}
+        <img 
+          src="https://www.leeveo.tv/wp-content/uploads/2024/09/image002.jpg" 
+          alt="Anne 2" 
+          className="w-full h-auto max-w-full object-cover"
+          style={{ maxHeight: '300px' }}
+        />
+        {/* Image 3 */}
+        <img 
+          src="https://www.leeveo.tv/wp-content/uploads/2024/09/image003.jpg" 
+          alt="Anne 3" 
+          className="w-full h-auto max-w-full object-cover"
+          style={{ maxHeight: '300px' }}
+        />
       </div>
       <HistoryContainer location="sidebar" />
     </div>

41c1c60cbf625120eaa0c2c4ea9983db87f6a7cd
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-14 12:31:44 +0200

    changement de nom

---
+++ components/right-sidebar.tsx
@@ -0,0 +1,30 @@
+import HistoryContainer from './history-container';
+
+export async function RightSidebar() {
+  return (
+    <div className="h-screen p-2 fixed top-0 right-0 flex flex-col justify-center pb-24 hidden sm:flex sidebar-container w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4"
+         style={{ marginTop: '150px' }}>
+      <div className="flex flex-col items-center space-y-4 w-full">
+        {/* Image 1 */}
+        <img 
+          src="https://www.leeveo.tv/wp-content/uploads/2024/09/image001.jpg" 
+          alt="Anne 1" 
+          className="sidebar-img w-full h-auto object-cover"
+        />
+        {/* Image 2 */}
+        <img 
+          src="https://www.leeveo.tv/wp-content/uploads/2024/09/image002.jpg" 
+          alt="Anne 2" 
+          className="sidebar-img w-full h-auto object-cover"
+        />
+        {/* Image 3 */}
+        <img 
+          src="https://www.leeveo.tv/wp-content/uploads/2024/09/image003.jpg" 
+          alt="Anne 3" 
+          className="sidebar-img w-full h-auto object-cover"
+        />
+      </div>
+      <HistoryContainer location="right-sidebar" />
+    </div>
+  );
+}

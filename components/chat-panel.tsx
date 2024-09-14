a0ccb746fe07c2b5e300c6f8484e044e39b4ad4c
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-14 12:08:23 +0200

    responsive

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -27,13 +27,12 @@ export function ChatPanel({ messages, query }: ChatPanelProps) {
   const { submit } = useActions()
   const router = useRouter()
   const inputRef = useRef<HTMLTextAreaElement>(null)
-  const isFirstRender = useRef(true) // For development environment
+  const isFirstRender = useRef(true)
 
   async function handleQuerySubmit(query: string, formData?: FormData) {
     setInput(query)
     setIsGenerating(true)
 
-    // Add user message to UI state
     setMessages(currentMessages => [
       ...currentMessages,
       {
@@ -42,7 +41,6 @@ export function ChatPanel({ messages, query }: ChatPanelProps) {
       }
     ])
 
-    // Submit and get response message
     const data = formData || new FormData()
     if (!formData) {
       data.append('input', query)
@@ -57,13 +55,11 @@ export function ChatPanel({ messages, query }: ChatPanelProps) {
     await handleQuerySubmit(input, formData)
   }
 
-  // if query is not empty, submit the query
   useEffect(() => {
     if (isFirstRender.current && query && query.trim().length > 0) {
       handleQuerySubmit(query)
       isFirstRender.current = false
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [query])
 
   useEffect(() => {
@@ -73,7 +69,6 @@ export function ChatPanel({ messages, query }: ChatPanelProps) {
     }
   }, [aiMessage, setIsGenerating])
 
-  // Clear messages
   const handleClear = () => {
     setIsGenerating(false)
     setMessages([])
@@ -83,11 +78,10 @@ export function ChatPanel({ messages, query }: ChatPanelProps) {
   }
 
   useEffect(() => {
-    // focus on input when the page loads
     inputRef.current?.focus()
   }, [])
 
-  // If there are messages and the new button has not been pressed, display the new Button
+  // Si des messages existent, afficher le bouton "Nouveau"
   if (messages.length > 0) {
     return (
       <div className="fixed bottom-2 md:bottom-8 left-0 right-0 flex justify-center items-center mx-auto pointer-events-none">
@@ -114,10 +108,10 @@ export function ChatPanel({ messages, query }: ChatPanelProps) {
   return (
     <div
       className={
-        'fixed bottom-8 left-0 right-0 top-10 mx-auto h-screen flex flex-col items-center justify-center'
+        'fixed bottom-8 left-0 right-0 top-10 mx-auto flex flex-col items-center justify-center h-full w-full max-w-[calc(100%-300px)] md:max-w-[calc(100%-450px)]'
       }
     >
-      <form onSubmit={handleSubmit} className="max-w-2xl w-full px-6">
+      <form onSubmit={handleSubmit} className="w-full px-6 max-w-lg">
         <div className="relative flex items-center w-full">
           <Textarea
             ref={inputRef}
@@ -128,19 +122,13 @@ export function ChatPanel({ messages, query }: ChatPanelProps) {
             placeholder="Discutons de la Bretagne..."
             spellCheck={false}
             value={input}
-            className="resize-none w-full min-h-12 rounded-fill bg-muted border border-input pl-4 pr-10 pt-3 pb-1 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50'"
+            className="resize-none w-full min-h-12 rounded-fill bg-muted border border-input pl-4 pr-10 pt-3 pb-1 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
             onChange={e => {
               setInput(e.target.value)
               setShowEmptyScreen(e.target.value.length === 0)
             }}
             onKeyDown={e => {
-              // Enter should submit the form
-              if (
-                e.key === 'Enter' &&
-                !e.shiftKey &&
-                !e.nativeEvent.isComposing
-              ) {
-                // Prevent the default action to avoid adding a new line
+              if (e.key === 'Enter' && !e.shiftKey && !e.nativeEvent.isComposing) {
                 if (input.trim().length === 0) {
                   e.preventDefault()
                   return
@@ -151,21 +139,12 @@ export function ChatPanel({ messages, query }: ChatPanelProps) {
               }
             }}
             onHeightChange={height => {
-              // Ensure inputRef.current is defined
               if (!inputRef.current) return
-
-              // The initial height and left padding is 70px and 2rem
               const initialHeight = 70
-              // The initial border radius is 32px
               const initialBorder = 32
-              // The height is incremented by multiples of 20px
               const multiple = (height - initialHeight) / 20
-
-              // Decrease the border radius by 4px for each 20px height increase
               const newBorder = initialBorder - 4 * multiple
-              // The lowest border radius will be 8px
-              inputRef.current.style.borderRadius =
-                Math.max(8, newBorder) + 'px'
+              inputRef.current.style.borderRadius = Math.max(8, newBorder) + 'px'
             }}
             onFocus={() => setShowEmptyScreen(true)}
             onBlur={() => setShowEmptyScreen(false)}

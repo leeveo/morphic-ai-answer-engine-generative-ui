61016692ffde35d357f0580ea810de75aa5fc496
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-14 10:39:24 +0200

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,32 +1,30 @@
-import HistoryContainer from './history-container'
+import HistoryContainer from './history-container';
 
 export async function Sidebar() {
   return (
-    <div className="h-screen p-2 fixed top-0 left-0 flex-col justify-center pb-24 hidden sm:flex" style={{ width: '450px', marginTop: '100px' }}>
-      <div className="flex flex-col items-center space-y-4">
+    <div className="h-screen p-2 fixed top-0 left-0 flex flex-col justify-center pb-24 hidden sm:flex" 
+         style={{ maxWidth: '100%', marginTop: '100px' }}>
+      <div className="flex flex-col items-center space-y-4 w-full">
         {/* Image 1 */}
         <img 
           src="https://www.leeveo.tv/wp-content/uploads/2024/09/image001.jpg" 
           alt="Anne 1" 
-          className="w-full h-auto max-w-full object-cover"
-          style={{ maxHeight: '300px' }}  // Limite la hauteur si nécessaire
+          className="w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl object-cover"
         />
         {/* Image 2 */}
         <img 
           src="https://www.leeveo.tv/wp-content/uploads/2024/09/image002.jpg" 
           alt="Anne 2" 
-          className="w-full h-auto max-w-full object-cover"
-          style={{ maxHeight: '300px' }}
+          className="w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl object-cover"
         />
         {/* Image 3 */}
         <img 
           src="https://www.leeveo.tv/wp-content/uploads/2024/09/image003.jpg" 
           alt="Anne 3" 
-          className="w-full h-auto max-w-full object-cover"
-          style={{ maxHeight: '300px' }}
+          className="w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl object-cover"
         />
       </div>
       <HistoryContainer location="sidebar" />
     </div>
-  )
+  );
 }

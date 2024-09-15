901549cff295c143cc422152b18985ecbaf767d0
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-15 20:22:11 +0200

    ajout image footer pour smartphone

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,11 +3,23 @@ import Link from 'next/link'
 import { SiInstagram, SiLinkedin } from 'react-icons/si'
 import { MdOutlineWebAsset } from 'react-icons/md'
 import { Button } from './ui/button'
+import Image from 'next/image'
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full p-4 md:p-6 bg-gray-900 text-white fixed bottom-0 left-0">
+    <footer className="w-full p-4 md:p-6 bg-white-900 text-white fixed bottom-0 left-0">
       <div className="flex flex-col items-center md:items-end justify-between">
+        {/* Image affichée uniquement sur les petits écrans */}
+        <div className="block sm:hidden mb-4">
+          <Image
+            src="/images/ton-image.png" // Chemin vers ton image dans le dossier public/images
+            alt="Image pour les petits écrans"
+            width={150} // Largeur de l'image (à ajuster)
+            height={50} // Hauteur de l'image (à ajuster)
+            className="object-contain"
+          />
+        </div>
+
         {/* Liens réseaux sociaux */}
         <div className="flex space-x-4 justify-center md:justify-end">
           <Button variant={'ghost'} size={'icon'} className="text-white/70">
@@ -26,6 +38,7 @@ const Footer: React.FC = () => {
             </Link>
           </Button>
         </div>
+
         {/* Texte du footer */}
         <p className="mt-2 text-xs text-center md:text-right">
           Suivez moi sur les réseaux sociaux - Propulsé par adeliade.ai

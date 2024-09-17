a5b57ea46b4bed310aa75b8a703fc8ba0116bbb9
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-17 16:26:15 +0200

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,11 +1,11 @@
-import SectionContainer from './section';
+import { SearchResults } from './search-results';
 
 export async function Sidebar() {
   return (
     <div className="h-screen p-2 fixed top-0 left-0 flex flex-col justify-center pb-24 hidden sm:flex sidebar-container w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4"
          style={{ marginTop: '50px' }}>
      
-      <SectionContainer location="sidebar" />
+      <SearchResults location="sidebar" />
     </div>
   );
 }

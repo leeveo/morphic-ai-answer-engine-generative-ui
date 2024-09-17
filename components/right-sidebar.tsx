e4cf65dee05a936231b2109ac45d261fd39d52b1
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-17 21:46:15 +0200

    upfdate

--- components/right-sidebar.tsx
+++ components/right-sidebar.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import { SearchSection } from './search-section';
+import HistoryContainer from './history-container';
 
-const RightSidebar = () => {
-  const result = []; // Remplacez ceci par vos données streamables
-  const includeDomains = ['example.com', 'anotherdomain.com'];
-
+export async function RightSidebar() {
   return (
-    <div className="right-sidebar">
-      <SearchSection result={result} includeDomains={includeDomains} />
-      {/* Autres composants de la barre latérale droite */}
+    <div className="h-screen p-2 fixed top-0 right-0 flex flex-col justify-center pb-24 hidden sm:flex sidebar-container w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4"
+         style={{ marginTop: '50px' }}>
+      <div className="flex flex-col items-center space-y-4 w-full">
+        {/* Image 1 */}
+        <img 
+          src="https://www.leeveo.tv/wp-content/uploads/2024/09/image001.jpg" 
+          alt="Anne 1" 
+          className="sidebar-img w-full max-w-full h-auto object-cover"
+        />
+        {/* Image 2 */}
+        <img 
+          src="https://www.leeveo.tv/wp-content/uploads/2024/09/image002.jpg" 
+          alt="Anne 2" 
+          className="sidebar-img w-full max-w-full h-auto object-cover"
+        />
+        {/* Image 3 */}
+        <img 
+          src="https://www.leeveo.tv/wp-content/uploads/2024/09/image003.jpg" 
+          alt="Anne 3" 
+          className="sidebar-img w-full max-w-full h-auto object-cover"
+        />
+      </div>
+      <HistoryContainer location="sidebar" />
     </div>
   );
-};
-
-export default RightSidebar;
+}

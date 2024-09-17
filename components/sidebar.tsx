ffa6f1274bc606115dd5971dd833081c46651be1
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-17 10:42:20 +0200

    upadte

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,29 +1,19 @@
+import { SearchResultsImageSection } from './search-results-image'; // Import de la section des images
 import HistoryContainer from './history-container';
+import type { SearchResultImage } from '@/lib/types';
 
-export async function Sidebar() {
+export async function Sidebar({ images }: { images: SearchResultImage[] }) {
   return (
     <div className="h-screen p-2 fixed top-0 left-0 flex flex-col justify-center pb-24 hidden sm:flex sidebar-container w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4"
          style={{ marginTop: '50px' }}>
-      <div className="flex flex-col items-center space-y-4 w-full">
-        {/* Image 1 */}
-        <img 
-          src="https://www.leeveo.tv/wp-content/uploads/2024/09/image001.jpg" 
-          alt="Anne 1" 
-          className="sidebar-img w-full max-w-full h-auto object-cover"
-        />
-        {/* Image 2 */}
-        <img 
-          src="https://www.leeveo.tv/wp-content/uploads/2024/09/image002.jpg" 
-          alt="Anne 2" 
-          className="sidebar-img w-full max-w-full h-auto object-cover"
-        />
-        {/* Image 3 */}
-        <img 
-          src="https://www.leeveo.tv/wp-content/uploads/2024/09/image003.jpg" 
-          alt="Anne 3" 
-          className="sidebar-img w-full max-w-full h-auto object-cover"
-        />
+      {/* Intégration de la section images */}
+      <div className="w-full space-y-4">
+        <h2 className="text-center text-lg font-bold">Résultats de la recherche</h2>
+        {/* Composant SearchResultsImageSection pour afficher les images */}
+        <SearchResultsImageSection images={images} query="Recherche utilisateur" />
       </div>
+
+      {/* Historique */}
       <HistoryContainer location="sidebar" />
     </div>
   );

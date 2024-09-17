ec7a8b40e6dd20be76a9e12c3fe4bd9ee5c2dbf1
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-17 09:39:17 +0200

    image a gauche

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,29 +1,34 @@
+/* eslint-disable @next/next/no-img-element */
+'use client'
+
+import { useEffect, useState } from 'react';
+import { SearchResultsImageSection } from './search-results-image';
 import HistoryContainer from './history-container';
+import { SearchResultImage } from '@/lib/types'; // S'assurer que le type est bien importé
 
 export async function Sidebar() {
+  const [images, setImages] = useState<SearchResultImage[]>([]);
+
+  useEffect(() => {
+    // Remplacer par ton appel API pour récupérer les images dynamiques
+    async function fetchImages() {
+      const fetchedImages = await fetch('/api/images').then(res => res.json());
+      setImages(fetchedImages);
+    }
+
+    fetchImages();
+  }, []);
+
   return (
     <div className="h-screen p-2 fixed top-0 left-0 flex flex-col justify-center pb-24 hidden sm:flex sidebar-container w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4"
          style={{ marginTop: '50px' }}>
-      <div className="flex flex-col items-center space-y-4 w-full">
-        {/* Image 1 */}
-        <img 
-          src="https://www.leeveo.tv/wp-content/uploads/2024/09/image001.jpg" 
-          alt="Anne 1" 
-          className="sidebar-img w-full max-w-full h-auto object-cover"
-        />
-        {/* Image 2 */}
-        <img 
-          src="https://www.leeveo.tv/wp-content/uploads/2024/09/image002.jpg" 
-          alt="Anne 2" 
-          className="sidebar-img w-full max-w-full h-auto object-cover"
-        />
-        {/* Image 3 */}
-        <img 
-          src="https://www.leeveo.tv/wp-content/uploads/2024/09/image003.jpg" 
-          alt="Anne 3" 
-          className="sidebar-img w-full max-w-full h-auto object-cover"
-        />
+      {/* Carrousel d'images dynamiques */}
+      <div className="w-full space-y-4">
+        <h2 className="text-center text-lg font-bold">Résultats de la recherche</h2>
+        <SearchResultsImageSection images={images} query="Recherche utilisateur" />
       </div>
+
+      {/* Historique */}
       <HistoryContainer location="sidebar" />
     </div>
   );

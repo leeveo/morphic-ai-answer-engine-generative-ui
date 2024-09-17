74635e1124bd9109e81843081f56400a3f590f29
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-17 16:18:50 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,7 +67,7 @@ export default function RootLayout({
                 
                 {/* Colonne 1 : Occupant 300px de largeur */}
                 <div className="w-[300px]">
-                  <p>Contenu de la première colonne (420px)</p>
+                  <p>Contenu de la première colonne (620px)</p>
                 </div>
 
                 {/* Colonne 2 : Occupant 680px de largeur */}

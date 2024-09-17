2c37949dab025a402daaad2c0eb0b5e64f1cdeb4
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-17 14:19:03 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,47 @@
+import type { Metadata, Viewport } from 'next'
+import { Inter as FontSans } from 'next/font/google'
+import './globals.css'
+import { cn } from '@/lib/utils'
+import { ThemeProvider } from '@/components/theme-provider'
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+import { Sidebar } from '@/components/sidebar'
+import { RightSidebar } from '@/components/right-sidebar' // Importer la sidebar droite
+import { Toaster } from '@/components/ui/sonner'
+import { AppStateProvider } from '@/lib/utils/app-state'
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans'
+})
+
+const title = 'Adeliade'
+const description =
+  'ChatBot de Anne Kerdi, passionnee de Bretagne, je partage mes decouvertes.'
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://adeliade.ai'),
+  title,
+  description,
+  openGraph: {
+    title,
+    description
+  },
+  twitter: {
+    title,
+    description,
+    card: 'summary_large_image',
+    creator: '@leeveo'
+  }
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1
+}
+
 export default function RootLayout({
   children
 }: Readonly<{

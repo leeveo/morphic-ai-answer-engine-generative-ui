483a6673c19e16df8d380dcadcf0e73eb2cb4823
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-17 11:32:56 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,53 @@
+import type { Metadata, Viewport } from 'next'
+import { Inter as FontSans } from 'next/font/google'
+import './globals.css'
+import { cn } from '@/lib/utils'
+import { ThemeProvider } from '@/components/theme-provider' // Vérifiez si le chemin est correct
+import Header from '@/components/header' // Assurez-vous que le chemin est correct
+import Footer from '@/components/footer' // Assurez-vous que le chemin est correct
+import { Sidebar } from '@/components/sidebar' // Assurez-vous que le chemin est correct
+import { RightSidebar } from '@/components/right-sidebar' // Assurez-vous que le chemin est correct
+import { Toaster } from '@/components/ui/sonner' // Assurez-vous que le chemin est correct
+import { AppStateProvider } from '@/lib/utils/app-state' // Vérifiez si le chemin est correct
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans'
+})
+
+const title = 'Adeliade'
+const description =
+  'ChatBot de Anne Kerdi, passionnee de Bretagne, je partage mes decouvertes.'
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://adeliade.ai'),
+  title,
+  description,
+  openGraph: {
+    title,
+    description
+  },
+  twitter: {
+    title,
+    description,
+    card: 'summary_large_image',
+    creator: '@leeveo'
+  }
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1
+}
+
 export default function RootLayout({
   children,
-  result // Assurez-vous de passer "result" en tant que prop ou de le récupérer dynamiquement ici.
+  result // Ajoutez result si nécessaire ici
 }: Readonly<{
   children: React.ReactNode
-  result: any // Assurez-vous que le type correspond à celui attendu dans Sidebar.
+  result?: any // Modifiez le type en fonction des données que vous attendez
 }>) {
   return (
     <html lang="fr" suppressHydrationWarning>

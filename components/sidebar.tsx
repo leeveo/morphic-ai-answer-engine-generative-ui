c66e79ec2585319b7606207c695e86679b7e9c4d
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-17 20:39:41 +0200

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,11 +1,13 @@
-import HistoryContainer from './history-container';
+import React, { ReactNode } from 'react'
 
-export async function Sidebar() {
+interface SidebarProps {
+  children: ReactNode
+}
+
+export const Sidebar: React.FC<SidebarProps> = ({ children }) => {
   return (
-    <div className="h-screen p-2 fixed top-0 left-0 flex flex-col justify-center pb-24 hidden sm:flex sidebar-container w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4"
-         style={{ marginTop: '50px' }}>
-     
-      <HistoryContainer location="sidebar" />
-    </div>
-  );
+    <aside className="sidebar">
+      {children}
+    </aside>
+  )
 }

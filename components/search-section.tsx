1605f9d7998f76bd501a6bd9c9e383f5670b9279
Leeveo <101552154+leeveo@users.noreply.github.com>
2024-09-17 11:11:14 +0200

    update

--- components/search-section.tsx
+++ components/search-section.tsx
@@ -1,32 +1,33 @@
-'use client'
+'use client';
 
-import { SearchResults } from './search-results'
-import { DefaultSkeleton } from './default-skeleton'
-import { SearchResultsImageSection } from './search-results-image'
-import { Section } from './section'
-import { ToolBadge } from './tool-badge'
-import type { SearchResults as TypeSearchResults } from '@/lib/types'
-import { StreamableValue, useStreamableValue } from 'ai/rsc'
+import { SearchResults } from './search-results';
+import { DefaultSkeleton } from './default-skeleton';
+import { SearchResultsImageSection } from './search-results-image';
+import { Section } from './section';
+import { ToolBadge } from './tool-badge';
+import type { SearchResults as TypeSearchResults } from '@/lib/types';
+import { StreamableValue, useStreamableValue } from 'ai/rsc';
 
 export type SearchSectionProps = {
-  result?: StreamableValue<string>
-  includeDomains?: string[]
-}
+  result?: StreamableValue<string>;
+  includeDomains?: string[];
+};
 
 export function SearchSection({ result, includeDomains }: SearchSectionProps) {
-  const [data, error, pending] = useStreamableValue(result)
-  const searchResults: TypeSearchResults = data ? JSON.parse(data) : undefined
+  const [data, error, pending] = useStreamableValue(result);
+  const searchResults: TypeSearchResults = data ? JSON.parse(data) : undefined;
   const includeDomainsString = includeDomains
-    ?  [${includeDomains.join(', ')}]
-    : ''
+    ? ` [${includeDomains.join(', ')}]`
+    : '';
+
   return (
     <div>
       {!pending && data ? (
         <>
           <Section size="sm" className="pt-2 pb-0">
-            <ToolBadge tool="search">{${searchResults.query}${includeDomainsString}}</ToolBadge>
+            <ToolBadge tool="search">{`${searchResults?.query}${includeDomainsString}`}</ToolBadge>
           </Section>
-          {searchResults.images && searchResults.images.length > 0 && (
+          {searchResults?.images && searchResults.images.length > 0 && (
             <Section title="Images">
               <SearchResultsImageSection
                 images={searchResults.images}
@@ -35,12 +36,12 @@ export function SearchSection({ result, includeDomains }: SearchSectionProps) {
             </Section>
           )}
           <Section title="Sources">
-            <SearchResults results={searchResults.results} />
+            <SearchResults results={searchResults?.results} />
           </Section>
         </>
       ) : (
         <DefaultSkeleton />
       )}
     </div>
-  )
+  );
 }
